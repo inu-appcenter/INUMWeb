ea5e67eed79778cd8def575c28027cdd2aa98439
임동완 <limson_95@naver.com>
2019-10-11 00:23:21 +0900

    slick

--- src/container/MainItemList.jsx
+++ src/container/MainItemList.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import ItemCard from '../component/item/ItemCard'
+import Slider from 'react-slick'
 
 class MainItemList extends Component {
     render() {
+        var settings = {
+            dots: true,
+            infinite: false,
+            speed: 100,
+            slidesToShow: 4,
+            slidesToScroll: 4
+          };
         return (
-            <div className = "mainItemList">
+            <Slider {...settings}data-slick='{"slidesToShow": 4, "slidesToScroll": 4}' className = "mainItemList">
                 {this.props.resData.map((Data)=>{
                     return(<ItemCard key={Data.productId} productName={Data.productName} price={Data.productPrice}/>)
                 })}
-            </div>
+            </Slider>
         );
     }
 }
